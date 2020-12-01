7848a27dca8a8cd216d868b4e5479ab5140e61ce
Bastien Robert <bastienrobert@outlook.com>
2020-12-01 21:19:29 +0100

    feat: mobile dragging enabled

--- src/Experience/index.ts
+++ src/Experience/index.ts
@@ -92,27 +92,41 @@ export default class Experience extends Transform {
     document.addEventListener('mousedown', this._onMouseDown)
     document.addEventListener('mousemove', this._onMouseMove)
     document.addEventListener('mouseup', this._onMouseUp)
+    document.addEventListener('touchstart', this._onTouchStart)
+    document.addEventListener('touchmove', this._onTouchMove)
+    document.addEventListener('touchend', this._onMouseUp)
   }
 
   _unlisten() {
     document.removeEventListener('mousedown', this._onMouseDown)
     document.removeEventListener('mousemove', this._onMouseMove)
     document.removeEventListener('mouseup', this._onMouseUp)
+    document.removeEventListener('touchstart', this._onTouchStart)
+    document.removeEventListener('touchmove', this._onTouchMove)
+    document.removeEventListener('touchend', this._onMouseUp)
   }
 
-  _onMouseDown = () => {
+  _onMouseDown = (e: MouseEvent | Touch) => {
     this._mouse.z = 1
     this._mouseNorm.z = 1
     if (this._cube.isHit) this._cube.isDown.copy(this._mouseNorm)
   }
 
-  _onMouseMove = (e: MouseEvent) => {
+  _onTouchStart = (e: TouchEvent) => {
+    this._onMouseDown(e.touches[0])
+  }
+
+  _onMouseMove = (e: MouseEvent | Touch) => {
     this._mouse.set(e.clientX, e.clientY, this._mouse.z)
     getResolutionNormalizedCoords(
       this._mouse,
       this._resolution,
       this._mouseNorm,
     )
+  }
+
+  _onTouchMove = (e: TouchEvent) => {
+    this._onMouseMove(e.touches[0])
   }
 
   _onMouseUp = () => {
