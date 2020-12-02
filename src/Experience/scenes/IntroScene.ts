d712d317fa691d2657a8262d5411238051d0de76
Bastien Robert <bastienrobert@outlook.com>
2020-12-02 15:47:01 +0100

    refactor: SceneManager structure and main experience Scene organization

--- src/Experience/scenes/IntroScene.ts
+++ src/Experience/scenes/IntroScene.ts
@@ -1,6 +1,23 @@
 import { Camera, OGLRenderingContext, Transform, Vec2, Vec3 } from 'ogl'
+import {
+  applyMatrix4,
+  box,
+  cloneBox,
+  getIsIntersectedBoundingBox,
+} from '~/utils/box'
 
 import { Scene, SceneParams } from '../controllers/SceneController'
+
+import RaycastableMesh from '../core/RaycastableMesh'
+
+import Planes from '../groups/Planes'
+import Cube from '../meshes/Cube'
+
+interface IntroSceneParams extends SceneParams {
+  raycastable: RaycastableMesh[]
+}
+
+const tmp_cube_bound = box()
 
 export default class IntroScene extends Transform implements Scene {
   name = 'intro'
@@ -9,8 +26,15 @@ export default class IntroScene extends Transform implements Scene {
   _resolution: Vec2
   _mouse: Vec3
   _camera: Camera
+  _raycastable: RaycastableMesh[]
 
-  constructor(gl, { resolution, mouse, camera }: SceneParams) {
+  _cube: Cube
+  _planes: Planes
+
+  constructor(
+    gl,
+    { resolution, mouse, camera, raycastable }: IntroSceneParams,
+  ) {
     super()
 
     this._gl = gl
@@ -18,5 +42,51 @@ export default class IntroScene extends Transform implements Scene {
     this._resolution = resolution
     this._mouse = mouse
     this._camera = camera
+    this._raycastable = raycastable
+
+    this._cube = new Cube(this._gl, {
+      mouse: this._mouse,
+      camera: this._camera,
+      resolution: this._resolution,
+    })
+    this._raycastable.push(this._cube)
+    this.addChild(this._cube)
+    console.log(this._cube)
+
+    this._planes = new Planes(this._gl, {
+      camera: this._camera,
+      resolution: this._resolution,
+    })
+    this.addChild(this._planes)
+  }
+
+  onMouseDown = () => {
+    if (this._cube.isHit) this._cube.isDown.copy(this._mouse)
+  }
+
+  onMouseUp = () => {
+    this._cube.isDown.z = 0
+  }
+
+  resize = () => {
+    this._planes.resize()
+    this._cube.resize()
+  }
+
+  update = (t) => {
+    this._cube.update(t)
+    this._planes.update()
+
+    // check collisions
+    cloneBox(this._cube.geometry.bounds, tmp_cube_bound)
+    applyMatrix4(tmp_cube_bound, this._cube.worldMatrix)
+    this._planes.alpha.isCollide = getIsIntersectedBoundingBox(
+      this._planes.bounds.alpha,
+      tmp_cube_bound,
+    )
+    this._planes.beta.isCollide = getIsIntersectedBoundingBox(
+      this._planes.bounds.beta,
+      tmp_cube_bound,
+    )
   }
 }
