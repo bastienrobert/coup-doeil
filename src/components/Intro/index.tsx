84f2b1b8ce18bf5b565fe26f59faec73c2db58f2
melanie.opixido <melanie.terzic@gmail.com>
2020-12-03 18:06:38 +0100

    Style: Add intro breakpoints

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,27 +1,31 @@
 import { h } from 'preact'
-import { Howl, Howler } from 'howler';
-
-import css from './styles.module.scss'
+import { useCallback, useRef } from 'preact/hooks'
+import { Howl, Howler } from 'howler'
 
 import grandma from '~/assets/img/grandma.png'
 import dog from '~/assets/img/dog_intro.png'
 
+import { intro } from '~/sounds'
+
+import css from './styles.module.scss'
+
 export default function Intro() {
+  const isPlaying = useRef(false)
 
-  const sound = new Howl({
-    src: ['/sounds/intro.mp3'],
-    autoplay: false,
-    volume: 0.5
-  });
-
+  const onClick = useCallback(() => {
+    if (!isPlaying.current) {
+      isPlaying.current = true
+      intro.play()
+    }
+  }, [])
 
   return (
-    <div className={css.Intro}>
+    <div className={css.Intro} onClick={onClick}>
       <h1 className={css.h1}>mamie</h1>
       <img className={css.dog} src={dog} />
       <h2 className={css.h2}>coco</h2>
       <img className={css.grandma} src={grandma} />
-      <p className={css.p}>Clic pour commencer</p>
+      <p className={css.p}>Clique pour commencer</p>
     </div>
   )
 }
---
+++ src/sounds.ts
@@ -0,0 +1,7 @@
+import { Howl } from 'howler'
+
+export const intro = new Howl({
+  src: ['/sounds/intro.mp3'],
+  autoplay: false,
+  volume: 0.5
+});
